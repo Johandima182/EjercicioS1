97fec31f37d9eb348527da8d0532874fc72a9179
Johan Diaz <diazjohan21@gmail.com>
2021-10-30 10:12:49 -0500

    conexion a mongodb

--- Servidor.js
+++ Servidor.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//Conexion
+mongoose.connect('mongodb+srv://ProWeb:ProWebMintic@misionticciclo4.vei8m.mongodb.net/EjercicioSem1?retryWrites=true&w=majority')
 
 app.listen(3000, () => {
     console.log('servidor conectado a puerto 3000')
